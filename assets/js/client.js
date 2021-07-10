acb8ec0a10982f42886070178c3fa841fecdffbc
Decode Marines <goipukraine@gmail.com>
2021-07-10 18:42:10 +0300

    getUserMedia setup

--- assets/js/client.js
+++ assets/js/client.js
@@ -1,3 +1,19 @@
 var url_string = window.location.href;
 var url = new URL(url_string);
 var username = url.searchParams.get("username");
+
+var local_video = document.querySelector("#local-video");
+
+// navigator.getUserMedia(constraint, success_cb,error_cb);
+
+navigator.getUserMedia({
+    video: true,
+            audio: true
+        }, function (myStream) {
+            stream = myStream;
+        local_video.srcObject = stream;
+        // asks for permition to use camera
+        // video icon on the right
+}, function (error) {
+    console.log(error) // DOMException: Permission denied
+});
